281afa349c8204cebb42884471a9b8c1a88abe2f
LegendRK <rgk8966@g.rit.edu>
2017-12-14 21:28:37 -0500

    Made the jukebox play a song after animation. Replaced some placeholders. Fixed some animations

--- Final Level Design Project/Assets/Scripts/Trigger.cs
+++ Final Level Design Project/Assets/Scripts/Trigger.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Trigger : MonoBehaviour {
 
     public GameObject obj;
+	private bool playedMusic = false;
+
     void OnTriggerEnter(Collider other)
     {
         obj.GetComponent<Animator>().SetBool("Entered", true);
-        Debug.Log("Entered Diner");
+        //Debug.Log("Entered Diner");
     }
+	void OnTriggerExit(Collider other)
+	{
+		obj.GetComponent<Animator>().SetBool("Entered", false);
+	}
     void Update()
     {
         AnimatorStateInfo state = obj.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0);
@@ -17,9 +23,21 @@ public class Trigger : MonoBehaviour {
             obj.GetComponent<Animator>().SetBool("Entered", false);
 		if (state.IsName ("Idle") ) 
 		{
-			if(Input.GetKeyDown(KeyCode.E) && obj.GetComponent<Animator>().GetBool("Entered") == true)
-				obj.GetComponent<Animator>().SetBool("Triggered", true);
+			if (Input.GetKeyDown (KeyCode.E) && obj.GetComponent<Animator> ().GetBool ("Entered") == true) {
+				obj.GetComponent<Animator> ().SetBool ("Triggered", true);
+				// for jukebox playing music
+			}
 		}
+
+		if(obj.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Take 001") && obj.name == "jukebox_anim" && !playedMusic)
+		{
+			AudioSource audio = obj.GetComponent<AudioSource> ();
+			audio.Play (430000);
+
+			playedMusic = true;
+		}
+
+
     }
 
     
